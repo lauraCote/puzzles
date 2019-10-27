e75178ae149bfc50480dfab511e4d2ad8120e781
Laura <lcote@cortexmedia.ca>
2019-10-27 19:49:37 -0400

    first commit

---
+++ reverse-integer.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if (x > 2147483647 or x < -2147483648) {
+            return 0
+        }
+        str = x.str()
+        for s in str
