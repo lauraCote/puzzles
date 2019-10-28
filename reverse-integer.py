91e2fec73e7d30df9b354799096252d1a031ed9d
Laura Côté <lauracote871@gmail.com>
2019-10-27 22:07:11 -0400

    reverse-integer

--- reverse-integer.py
+++ reverse-integer.py
@@ -5,14 +5,14 @@ class Solution:
         rev: int = 0
         while x != 0:
             pop: int = x % 10
-            x /= 10
+            x //= 10
             if (rev > (2147483647 / 10)) or (rev == 2147483647 and pop > 7):
                 return 0
             if (rev < (-2147483648 / 10)) or (rev == -2147483648 and pop < -8):
                 return 0
             rev = rev * 10 + pop   
             print("rev " + str(rev))
-
+            print("x " + str(x))
         return rev
        
 
