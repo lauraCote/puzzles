06adfc91bd336fe48040a3d53305e092d9383b59
Laura Côté <lauracote871@gmail.com>
2019-10-27 22:02:27 -0400

    reverse-integer

--- reverse-integer.py
+++ reverse-integer.py
@@ -1,7 +1,19 @@
+import sys
+
 class Solution:
     def reverse(self, x: int) -> int:
-        if (x > 2147483647 or x < -2147483648) {
-            return 0
-        }
-        str = x.str()
-        for s in str
+        rev: int = 0
+        while x != 0:
+            pop: int = x % 10
+            x /= 10
+            if (rev > (2147483647 / 10)) or (rev == 2147483647 and pop > 7):
+                return 0
+            if (rev < (-2147483648 / 10)) or (rev == -2147483648 and pop < -8):
+                return 0
+            rev = rev * 10 + pop   
+            print("rev " + str(rev))
+
+        return rev
+       
+
+print(Solution().reverse(123))
